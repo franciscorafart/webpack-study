54d2548cea4e1b0c47a1a20fac9297a351be560d
Francisco Rafart <francisco.rafart@gmail.com>
2018-08-15 19:50:34 -0400

    loaders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './bulba.png';
 
 function component(){
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    //add css class to element
+    element.classList.add('hello');
+
+    let myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon)
 
     return element;
 }
